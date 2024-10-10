259a35bc194873d256223c1486414ffabdde6061
haiderfiaz01945 <haiderfiaz09@gmail.com>
2024-10-11 01:21:57 +0500

    vjg

--- src/Pages/AdminDashboard.jsx
+++ src/Pages/AdminDashboard.jsx
@@ -182,7 +182,7 @@ function AdminDashboard() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1, transition: { duration: 0.5, delay: 0.1 } }}
             >
-              Namee
+              Name
             </motion.label>
             <motion.input
   type="text"
